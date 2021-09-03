4b27f0026126e7046a54bae22193230c33df2ebb
herb@wks02 <herbert.kreutzer@sdt.co.at>
2021-09-03 12:29:57 +0200

    introduce karibu free MainView2

--- src/main/kotlin/com/vaadin/flow/demo/helloworld/MainView2.kt
+++ src/main/kotlin/com/vaadin/flow/demo/helloworld/MainView2.kt
@@ -15,49 +15,49 @@
  */
 package com.vaadin.flow.demo.helloworld
 
-import com.github.mvysny.karibudsl.v10.*
 import com.vaadin.flow.component.Key
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.dependency.CssImport
 import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.router.Route
-import com.vaadin.flow.server.PWA
 
 /**
  * The main view contains a button and a click listener.
  */
-@Route("")
-@PWA(name = "Project Base for Vaadin", shortName = "Project Base")
-@CssImport.Container(value = [  // repeatable annotations are not supported by Kotlin, please vote for https://youtrack.jetbrains.com/issue/KT-12794
-    CssImport("./styles/shared-styles.css"),
-    CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-])
-class MainView : KComposite() {
-    private lateinit var nameField: TextField
+@Route("2")
+@CssImport.Container(
+    value = [  // repeatable annotations are not supported by Kotlin, please vote for https://youtrack.jetbrains.com/issue/KT-12794
+        CssImport("./styles/shared-styles.css"),
+        CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+    ]
+)
+class MainView2 : VerticalLayout() {
+    private var nameField: TextField = TextField("Your name2")
     private lateinit var greetButton: Button
 
-    // The main view UI definition
-    private val root = ui {
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        verticalLayout(classNames = "centered-content") {
-
-            // Use TextField for standard text input
-            nameField = textField("Your name")
-
-            // Use Button for a clickable button
-            greetButton = button("Say hello") {
-                setPrimary(); addClickShortcut(Key.ENTER)
-            }
-        }
-    }
 
     init {
+        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
+        addClassName("centered-content")
+
+        // Use TextField for standard text input
+
+        // Use Button for a clickable button
+        greetButton = Button("Say hello2")
+        {
+            // ? this.addThemeVariants(ButtonVariant.LUMO_PRIMARY)
+            addClickShortcut(Key.ENTER)
+        }
+
+        add(nameField, greetButton)
+
         // attach functionality to the UI components.
         // It's a good practice to keep UI functionality separated from UI definition.
 
         // Button click listeners can be defined as lambda expressions
-        greetButton.onLeftClick {
+        greetButton.addClickListener {
             // Notification.show("Hello, ${nameField.value}")
             Notification.show("Hello, ${nameField.value}", 5000, Notification.Position.MIDDLE)
         }
