f35f04bea49b696c892b030f55db2fb4148cf7f1
herb.nb-herb <herbert.kreutzer@gmail.com>
2021-09-06 17:47:53 +0200

    introduce Dialog

---
+++ src/main/kotlin/at.co.sdt.herb/MainView5.kt
@@ -0,0 +1,52 @@
+package at.co.sdt.herb
+
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.dependency.CssImport
+import com.vaadin.flow.component.dialog.Dialog
+import com.vaadin.flow.component.html.Paragraph
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.FlexLayout
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.router.Route
+
+@Route("5 dialog")
+@CssImport.Container(
+    value = [  // repeatable annotations are not supported by Kotlin, please vote for https://youtrack.jetbrains.com/issue/KT-12794
+        CssImport("./styles/shared-styles.css"),
+        CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+    ]
+)
+class MainView5 : VerticalLayout() {
+    val btnDialog = Button("Dialog")
+    val confirmDialog = ConfirmDialog()
+
+    init {
+        btnDialog.addClickListener {
+            confirmDialog.open()
+        }
+        add(btnDialog)
+    }
+}
+
+class ConfirmDialog : Dialog() {
+    val confirmText = Paragraph("Are you sure you want to leave this page?")
+    private val confirmButton = Button("Confirm")
+    private val cancelButton = Button("Cancel")
+
+    // Add a FlexLayout wrapper for the element you want to right align
+    private val cancelButtonWrapper = FlexLayout(cancelButton)
+
+    // Create the HorizontalLayout and expand the wrapper component
+    private val dialogButtons = HorizontalLayout(confirmButton, cancelButtonWrapper)
+
+    init {
+        // Set the wrapper flex alignment to end (right)
+        cancelButtonWrapper.setJustifyContentMode(FlexComponent.JustifyContentMode.END)
+        dialogButtons.expand(cancelButtonWrapper)
+
+        // Add the components to the dialog
+        add(confirmText, dialogButtons)
+    }
+}
